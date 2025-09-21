a3033f3a82a0c8ee859f6efebce67da172ff77c1
dansubaie2 <dansubaie029@gmail.com>
2025-09-21 11:48:32 +0300

    add grade to student and add getting average method

--- student/student.py
+++ student/student.py
@@ -4,8 +4,39 @@ class Student:
         self.name = name
         self.grades = []
 
-# we do this so the prompt doesnt show in the code
+    def add_grade(self, grade):
+        # Method to add a grade (0 to100 only). Invalid grades should raise an error.
+        if grade >= 0 and grade <= 100:
+            self.grades.append(grade)
+        else:
+            raise ValueError('Grade Must be between 0 and 100 inclusive')
+        
+
+    def average(self):
+        # Method to calculate the average of grades. If no grades, return 0.
+        if not self.grades:
+            return 0
+        
+        total = 0
+        for grade in self.grades:
+            total += grade
+        average = total / len(self.grades)
+        return average
+
+        
+
+# we do this so the prompt doesnt show in the terminal
 if __name__ == '__main__':
+    # this is where the user faceing "program" goes
     student_name = input('Enter the new student name: ')
     new_student = Student(student_name)
     print(f'welcome {new_student.name}')
+    new_grade = input('Add grade to student: ')
+    try:
+        new_student.add_grade(int(new_grade))
+    except ValueError as err:
+        # To get the error message we included on line 12
+        #instead of doing this manuallay :print('Nope You entered a number outside of 0 and 100')
+        print(err)
+
+    
--- tests/test_student.py
+++ tests/test_student.py
@@ -4,7 +4,38 @@ import unittest
 from student.student import Student
 
 class TestStudent(unittest.TestCase):
+    # setup function to setup our student
+    def setUp(self):
+        self.student = Student('Danah')
 
     def test_create_student_object(self):
-        new_student = Student('Danah')
-        self.assertEqual(new_student.name, 'Danah')
+        # new_student = Student('Danah')
+        self.assertEqual(self.student.name, 'Danah')
+
+    def test_add_valid_grade(self):
+        self.student.add_grade(50)
+        self.assertIn(50, self.student.grades)
+        self.student.add_grade(100)
+        self.assertIn(100, self.student.grades)
+        self.student.add_grade(0)
+        self.assertIn(0, self.student.grades)
+
+    def test_add_invalid_grade(self):
+        with self.assertRaises(ValueError):
+            self.student.add_grade(101)
+
+        with self.assertRaises(TypeError):
+            self.student.add_grade('hello')
+
+        with self.assertRaises(ValueError):
+            self.student.add_grade(-1)
+
+    
+    def test_average_no_grades(self):
+        self.assertEqual(self.student.average(), 0)
+    
+    def test_average_with_grades(self):
+        for grade in [20, 40, 60]:
+            self.student.add_grade(grade)
+        # i have to calculate the average myself
+        self.assertEqual(self.student.average(), 40)
